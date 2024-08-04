2c2e2e105fdc70cce943dcc5a0c9e98030831796
Lakhdar Belkharroubi <83500639+lakhdar-blk@users.noreply.github.com>
2024-08-04 10:31:49 +0100

    Update settings.py

--- environment/portapp/portapp/settings.py
+++ environment/portapp/portapp/settings.py
@@ -21,7 +21,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h2bo-kfn=^0#@m44+s5)8hgt2n2r49jwyn(a*+3l6s02&#+jl7'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
